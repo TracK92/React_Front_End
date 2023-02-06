bddee33c40c432e75a6b1e232ea5738b992b64ce
kadenyi <treykadenyi@gmail.com>
2023-02-06 15:27:11 +0300

    Remove unnecessary code

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -8,50 +8,53 @@ const Form = () => {
 
   const [accessCode, setAccessCode] = React.useState("");
 
+  // fetch data from backend
 
-// fetch data from backend
-  
   const createAccessCode = async (e) => {
     e.preventDefault();
     const post = { phoneNumber: phoneNumber };
 
     try {
-      const res = await axios.post("http://localhost:4000/createNewAccessCode", post);
+      const res = await axios.post(
+        "http://localhost:4000/createNewAccessCode",
+        post
+      );
       console.log(res);
     } catch (err) {
       console.log(err);
     }
-  }
-  
+  };
+
   const validateAccessCode = async (e) => {
     e.preventDefault();
     const post = { phoneNumber: phoneNumber, accessCode: accessCode };
 
     try {
-      const res = await axios.post("http://localhost:4000/validateAccessCode", post);
+      const res = await axios.post(
+        "http://localhost:4000/validateAccessCode",
+        post
+      );
       console.log(res);
 
-      if(res.data["message"] === "Access code is invalid") {
+      if (res.data["message"] === "Access code is invalid") {
         alert("Access Code is valid");
-      }
-      else if(res.data["message"] === "User doesn't exist.") {
+      } else if (res.data["message"] === "User doesn't exist.") {
         alert("User doesn't exist.");
-      } else if (res.data["message"] === "Access code and phone number are required") {
+      } else if (
+        res.data["message"] === "Access code and phone number are required"
+      ) {
         alert("Access code and phone number are required");
       } else {
-        alert("Success")
+        alert("Success");
         // save phone number to local storage
         localStorage.setItem("phoneNumber", phoneNumber);
         setPhoneNumber("");
         setAccessCode("");
       }
-    }
-    catch (err) {
+    } catch (err) {
       console.log(err);
     }
-  }
-   
-
+  };
 
   return (
     <form>
@@ -68,8 +71,9 @@ const Form = () => {
         required
       />
       <p className="format">Format: +1-123-456-7890</p>
-      {/* <p className="format">Format: 123-45-678</p> */}
-      <button type="button" onClick={createAccessCode}>Create Access Code</button>
+      <button type="button" onClick={createAccessCode}>
+        Create Access Code
+      </button>
       {/* access code */}
       <label htmlFor="access_code">Enter Access Code</label>
       <input
@@ -85,7 +89,9 @@ const Form = () => {
         required
       />
       {/* submit button */}
-      <button type="button" onClick={validateAccessCode}>Validate Access Code</button>
+      <button type="button" onClick={validateAccessCode}>
+        Validate Access Code
+      </button>
     </form>
   );
 };
