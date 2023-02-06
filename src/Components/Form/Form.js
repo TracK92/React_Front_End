dd13a51f6218edd70244a35c55706425143b8b54
kadenyi <treykadenyi@gmail.com>
2023-02-06 13:15:44 +0300

    Fetch and display data from the backend

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,21 +1,72 @@
 import React from "react";
+import axios from "axios";
 
 import "./form.css";
 
 const Form = () => {
+  const [phoneNumber, setPhoneNumber] = React.useState("");
+
+  const [accessCode, setAccessCode] = React.useState("");
+
+
+// fetch data from backend
+  
+  const createAccessCode = async (e) => {
+    e.preventDefault();
+    const post = { phoneNumber: phoneNumber };
+
+    try {
+      const res = await axios.post("http://localhost:4000/createNewAccessCode", post);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  const validateAccessCode = async (e) => {
+    e.preventDefault();
+    const post = { phoneNumber: phoneNumber, accessCode: accessCode };
+
+    try {
+      const res = await axios.post("http://localhost:4000/validateAccessCode", post);
+      console.log(res);
+
+      if(res.data["message"] === "Access code is invalid") {
+        alert("Access Code is valid");
+      }
+      else if(res.data["message"] === "User doesn't exist.") {
+        alert("User doesn't exist.");
+      } else if (res.data["message"] === "Access code and phone number are required") {
+        alert("Access code and phone number are required");
+      } else {
+        alert("Success")
+        // save phone number to local storage
+        localStorage.setItem("phoneNumber", phoneNumber);
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+   
+
+
   return (
     <form>
       {/* phone number */}
       <label htmlFor="phone_number">Enter Phone Number</label>
       <input
-        type="tel"
+        type="text"
         id="phone_number"
         name="phone_number"
         placeholder="123-45-678"
-        pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+        // pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
         required
       />
       <p className="format">Format: 123-45-678</p>
+      <button type="button" onClick={createAccessCode}>Create Access Code</button>
       {/* access code */}
       <label htmlFor="access_code">Enter Access Code</label>
       <input
@@ -26,10 +77,12 @@ const Form = () => {
         maxLength="6"
         autoComplete="off"
         placeholder="123456"
+        value={accessCode}
+        onChange={(e) => setAccessCode(e.target.value)}
         required
       />
       {/* submit button */}
-      <input type="submit" value="Submit" />
+      <button type="button" onClick={validateAccessCode}>Validate Access Code</button>
     </form>
   );
 };
