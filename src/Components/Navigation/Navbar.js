5a9c3da775d1948b13cdf7208cb09db134a51459
kadenyi <treykadenyi@gmail.com>
2023-02-06 18:36:29 +0300

    Create navbar component

---
+++ src/Components/Navigation/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import "./navbar.css"
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <header>
+      <nav>
+        <ul className='navbar'>
+          <li>
+            <button onClick={() => navigate("/")}>
+              Access Codes
+            </button>
+          </li>
+          <li>
+           <button onClick={() => navigate("/users")}>
+              GitHub Users
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
