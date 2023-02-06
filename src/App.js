dfafe92313f781bc0f2e9776dbc9f419a2b4760f
kadenyi <treykadenyi@gmail.com>
2023-02-06 18:36:21 +0300

    Add navbar route

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Form from "./Components/Form/Form";
 import GitHubUsers from "./Components/GitHubUsers/GitHubUsers";
+import Navbar from "./Components/Navigation/Navbar";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/users" element={<GitHubUsers />} />
