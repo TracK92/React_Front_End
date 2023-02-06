9349d961aadcbe7369eb82721519c13168947b58
kadenyi <treykadenyi@gmail.com>
2023-02-06 15:51:24 +0300

    Add path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Form from "./Components/Form/Form";
+import GitHubUsers from "./Components/GitHubUsers/GitHubUsers";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Form />} />
+        <Route path="/users" element={<GitHubUsers />} />
       </Routes>
     </Router>
   );
