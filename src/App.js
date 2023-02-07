4279609ce65a1b9d734681a3280a1de1d6ad5d56
kadenyi <treykadenyi@gmail.com>
2023-02-07 12:14:10 +0300

    Add profile route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Form from "./Components/Form/Form";
 import GitHubUsers from "./Components/GitHubUsers/GitHubUsers";
+import Profile from "./Components/GitHubUsers/Profile";
 import Navbar from "./Components/Navigation/Navbar";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/users" element={<GitHubUsers />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
